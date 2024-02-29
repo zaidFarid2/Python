be2d5c5f06860ea13f03d9e2e21904ad690f639a
Muhammad Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2024-02-29 14:03:37 -0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+# class Employee:
+#     def __init__(self,name,age) -> None:
+#         self.name = name
+#         self.age = age
+
+#     def display(self):
+#         print(f"Employee name is {self.name}")
+#         print(f"Employee age is {self.age}")
+# e1 = Employee("zaid",29)
+# e1.display()
