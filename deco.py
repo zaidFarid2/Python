0be951d297c4420c593b3bef13975e370fc253e8
Muhammad Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2024-03-04 02:54:40 -0800

    Add files via upload

---
+++ deco.py
@@ -0,0 +1,48 @@
+# def my_decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()  # Call the original function
+#         print("Something is happening after the function is called.")
+#     return wrapper
+
+# @my_decorator
+# def say_hello():
+#     print("Hello!")
+
+# Call the decorated function
+# say_hello()
+
+
+
+# def mmeraDecu(func):
+#     def wrapper():
+#         print("beech me deco he")
+#         func()
+#         print("beech me deco he")
+#     return wrapper
+
+
+# @mmeraDecu
+# def kesaHe():
+#     print("me decorator hu")
+
+
+# kesaHe()
+
+
+
+
+
+# from os import write
+# from pickle import APPEND
+
+
+# file  =  open("name.txt","r")
+# #open the file
+# # file1 = read("name.txt")
+# # file2 = write("Hello world\n","w")
+# # file3.close()
+# # file 4 = APPEND("name.txt")
+# # open the file in append mode
+
+
