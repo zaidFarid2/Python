aa71d5d5da6a88fc09a7e5d2a785cbe37d4ccf25
Muhammad Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2024-03-14 10:33:31 -0700

    Add files via upload

---
+++ file.py
@@ -0,0 +1,31 @@
+# fp  = open("filename.txt ","r")
+# # fp  = write("filename,txt ","r")
+# print(fp)
+
+# with open("input.txt","r") as fp:
+#     print(fp.read())
+#     # print(fp)
+
+
+# with open('1.txt', 'w') as file:
+#     data = file.write("hye\n")
+#     data= file.write("bye")
+#     print(data)
+    # print(datar)
+
+# class Animal:
+#     def makeSound(self):
+#         pass
+
+# class Dog:
+#     def makeSound(self):
+#         print("bark")
+
+# class Cat:
+#     def makeSound(self):
+#         print("meow")
+
+# cat = Cat()
+# dog = Dog()
+# cat.makeSound()
+# dog .makeSound()
